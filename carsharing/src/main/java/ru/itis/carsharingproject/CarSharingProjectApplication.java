69d29454f5dd53ffecf22265319f654027949d01
AlexeyShandrenko <70972424+AlexeyShandrenko@users.noreply.github.com>
2021-05-10 17:12:46 +0300

    added spring boot project

---
+++ carsharing/src/main/java/ru/itis/carsharingproject/CarSharingProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.carsharingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingProjectApplication.class, args);
+    }
+
+}
---
+++ carsharing/src/test/java/ru/itis/carsharingproject/CarSharingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.carsharingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
